efcbc8899d8e7cdc7a74caa8109cc6ee609ec8ae
Abdulrahh <abdulrahmansurakat@gmail.com>
2025-04-11 12:20:11 +0100

    Add files via upload

---
+++ python_project_hangman_game/HangManGame_WordList.py
@@ -0,0 +1,33 @@
+# word list for handman game 
+
+
+words = ("elephant", "lion", "dolphin", "bat", "kangaroo",
+    "eagle", "penguin", "owl", "parrot", "flamingo",
+    "snake", "crocodile", "chameleon", "turtle", "komodo dragon",
+    "clownfish", "great white shark", "goldfish", "anglerfish", "swordfish",
+    "butterfly", "ant", "bee", "dragonfly", "beetle","giraffe", "zebra", "rhino", "hippopotamus", "cheetah",
+    "hawk", "woodpecker", "toucan", "swan", "robin",
+    "iguana", "gecko", "alligator", "monitor lizard", "python",
+    "pufferfish", "manta ray", "barracuda", "seahorse", "guppy",
+    "moth", "cricket", "grasshopper", "wasp", "ladybug","aardvark", "alpaca", "armadillo", "baboon", "badger",
+    "barramundi", "bison", "boar", "bonobo", "buffalo",
+    "bull", "camel", "capybara", "caracal", "cassowary",
+    "cat", "caterpillar", "centipede", "chimpanzee", "chipmunk",
+    "cougar", "cow", "coyote", "crab", "crane",
+    "cuscus", "deer", "dik-dik", "dingoe", "dog",
+    "donkey", "dugong", "duck", "eland", "emu",
+    "ferret", "finch", "firefly", "fox", "frog",
+    "gazelle", "gerbil", "gibbon", "goat", "goose",
+    "gorilla", "grass snake", "grouse", "guinea pig", "hamster",
+    "hare", "hedgehog", "heron", "hornet", "horse",
+    "hummingbird", "hyena", "jackal", "jaguar", "jellyfish",
+    "kiwi", "koala", "kudu", "lemming", "lemur",
+    "leopard", "lizard", "llama", "lobster", "lynx",
+    "macaw", "magpie", "manatee", "mandrill", "meerkat",
+    "mole", "mongoose", "monkey", "moose", "mosquito",
+    "mouse", "mule", "narwhal", "newt", "ocelot",
+    "octopus", "opossum", "orangutan", "oryx", "ostrich",
+    "otter", "ox", "panda", "panther", "peacock",
+    "pelican", "pheasant", "pig", "platypus", "porcupine")
+
+print(words)
